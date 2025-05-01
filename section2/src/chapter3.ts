1b0a733d231b2059cac61e84eabfbc65f9683f4c
yunadada <ynson0216@naver.com>
2025-05-01 20:11:38 +0900

    객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,6 @@
+// object
+let user: { id?: number; name: string } = { id: 1, name: "이정환" };
+user = { name: "홍길동" };
+// let dog: { name: string; color: string } = { name: "돌돌이", color: "brown" };
+
+let config: { readonly apiKey: string } = { apiKey: "MY API KEY" };
